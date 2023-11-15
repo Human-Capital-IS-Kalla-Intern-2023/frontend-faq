b54ebe20a663c68902bcbb685666c5f0c106fe1d
Muhthoriqas <muhthoriqas1719@gmail.com>
2023-11-15 15:35:43 +0800

    install package and libary, and also img

---
+++ src/assets/icons/icon.tsx
@@ -0,0 +1,264 @@
+interface SvgIconProps {
+  className?: string;
+  onClick?: () => void;
+}
+
+const ThreeDotIcon: React.FC<SvgIconProps> = ({ className }) => (
+  <svg
+    fill="currentColor"
+    viewBox="0 0 16 16"
+    height="1em"
+    width="1em"
+    className={className}
+  >
+    <path d="M9.5 13a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0zm0-5a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0zm0-5a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0z" />
+  </svg>
+);
+const EditIcon: React.FC<SvgIconProps> = ({ className }) => (
+  <svg
+    className={className}
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 20 20"
+    fill="currentColor"
+    aria-hidden="true"
+  >
+    <path d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z" />
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z"
+    />
+  </svg>
+);
+
+const DetailIcon: React.FC<SvgIconProps> = ({ className }) => (
+  <svg
+    className={className}
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 20 20"
+    fill="currentColor"
+    aria-hidden="true"
+  >
+    <path d="M10 12a2 2 0 100-4 2 2 0 000 4z" />
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M.458 10C1.732 5.943 5.522 3 10 3s8.268 2.943 9.542 7c-1.274 4.057-5.064 7-9.542 7S1.732 14.057.458 10zM14 10a4 4 0 11-8 0 4 4 0 018 0z"
+    />
+  </svg>
+);
+
+const TrashIcon: React.FC<SvgIconProps> = ({ className, onClick }) => (
+  <svg
+    fill="currentColor"
+    viewBox="0 0 16 16"
+    aria-hidden="true"
+    className={className}
+    onClick={onClick}
+  >
+    <path d="M2.5 1a1 1 0 00-1 1v1a1 1 0 001 1H3v9a2 2 0 002 2h6a2 2 0 002-2V4h.5a1 1 0 001-1V2a1 1 0 00-1-1H10a1 1 0 00-1-1H7a1 1 0 00-1 1H2.5zm3 4a.5.5 0 01.5.5v7a.5.5 0 01-1 0v-7a.5.5 0 01.5-.5zM8 5a.5.5 0 01.5.5v7a.5.5 0 01-1 0v-7A.5.5 0 018 5zm3 .5v7a.5.5 0 01-1 0v-7a.5.5 0 011 0z" />
+  </svg>
+);
+
+const LeftArrowIcon: React.FC<SvgIconProps> = ({ className }) => (
+  <svg
+    className={className}
+    aria-hidden="true"
+    fill="currentColor"
+    viewBox="0 0 20 20"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      fillRule="evenodd"
+      d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
+      clipRule="evenodd"
+    />
+  </svg>
+);
+
+const RightArrowIcon: React.FC<SvgIconProps> = ({ className }) => (
+  <svg
+    className={className}
+    aria-hidden="true"
+    fill="currentColor"
+    viewBox="0 0 20 20"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      fillRule="evenodd"
+      d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+      clipRule="evenodd"
+    />
+  </svg>
+);
+
+const SearchIcon: React.FC<SvgIconProps> = ({ className }) => (
+  <svg
+    className={className}
+    aria-hidden="true"
+    fill="currentColor"
+    viewBox="0 0 20 20"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      fillRule="evenodd"
+      d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+      clipRule="evenodd"
+    />
+  </svg>
+);
+
+const PlusIcon: React.FC<SvgIconProps> = ({ className }) => (
+  <svg
+    className={className}
+    fill="currentColor"
+    viewBox="0 0 20 20"
+    xmlns="http://www.w3.org/2000/svg"
+    aria-hidden="true"
+  >
+    <path
+      clipRule="evenodd"
+      fillRule="evenodd"
+      d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z"
+    />
+  </svg>
+);
+
+const ArrowButtonIcon: React.FC<SvgIconProps> = ({ className }) => (
+  <svg
+    className={className}
+    fill="currentColor"
+    viewBox="0 0 20 20"
+    xmlns="http://www.w3.org/2000/svg"
+    aria-hidden="true"
+  >
+    <path
+      clipRule="evenodd"
+      fillRule="evenodd"
+      d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+    />
+  </svg>
+);
+
+const LogoutIcon: React.FC<SvgIconProps> = ({ className }) => (
+  <svg
+    fill="currentColor"
+    viewBox="0 0 16 16"
+    height="23"
+    width="23"
+    className={className}
+  >
+    <path d="M8.5 10c-.276 0-.5-.448-.5-1s.224-1 .5-1 .5.448.5 1-.224 1-.5 1z" />
+    <path d="M10.828.122A.5.5 0 0111 .5V1h.5A1.5 1.5 0 0113 2.5V15h1.5a.5.5 0 010 1h-13a.5.5 0 010-1H3V1.5a.5.5 0 01.43-.495l7-1a.5.5 0 01.398.117zM11.5 2H11v13h1V2.5a.5.5 0 00-.5-.5zM4 1.934V15h6V1.077l-6 .857z" />
+  </svg>
+);
+
+const CloseSidebarIcon: React.FC<SvgIconProps> = ({ className }) => (
+  <svg
+    fill="currentColor"
+    viewBox="0 0 16 16"
+    height="25"
+    width="25"
+    className={className}
+  >
+    <path
+      fillRule="evenodd"
+      d="M12.5 15a.5.5 0 01-.5-.5v-13a.5.5 0 011 0v13a.5.5 0 01-.5.5zM10 8a.5.5 0 01-.5.5H3.707l2.147 2.146a.5.5 0 01-.708.708l-3-3a.5.5 0 010-.708l3-3a.5.5 0 11.708.708L3.707 7.5H9.5a.5.5 0 01.5.5z"
+    />
+  </svg>
+);
+const ReponsiveSidebarIcon: React.FC<SvgIconProps> = ({ className }) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    height="20"
+    width="20"
+    viewBox="0 0 448 512"
+    className={className}
+  >
+    <path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z" />
+  </svg>
+);
+
+const DropdownSidebarMenuIcon: React.FC<SvgIconProps> = ({ className }) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    height="1em"
+    viewBox="0 0 448 512"
+    className={className}
+  >
+    <path d="M201.4 342.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 274.7 86.6 137.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z" />
+  </svg>
+);
+
+const CloseButtonIcon: React.FC<SvgIconProps> = ({ className }) => (
+  <svg
+    fill="none"
+    viewBox="0 0 24 24"
+    height="1em"
+    width="1em"
+    className={className}
+  >
+    <path
+      fill="currentColor"
+      d="M6.225 4.811a1 1 0 00-1.414 1.414L10.586 12 4.81 17.775a1 1 0 101.414 1.414L12 13.414l5.775 5.775a1 1 0 001.414-1.414L13.414 12l5.775-5.775a1 1 0 00-1.414-1.414L12 10.586 6.225 4.81z"
+    />
+  </svg>
+);
+
+const LeftArrowIcon2: React.FC<SvgIconProps> = ({ className }) => (
+  <svg
+    fill="currentColor"
+    viewBox="0 0 16 16"
+    height="1em"
+    width="1em"
+    className={className}
+  >
+    <path
+      fillRule="evenodd"
+      d="M12 8a.5.5 0 01-.5.5H5.707l2.147 2.146a.5.5 0 01-.708.708l-3-3a.5.5 0 010-.708l3-3a.5.5 0 11.708.708L5.707 7.5H11.5a.5.5 0 01.5.5z"
+    />
+  </svg>
+);
+
+const RefreshIcon: React.FC<SvgIconProps> = ({ className }) => (
+  <svg
+    viewBox="0 0 24 24"
+    fill="currentColor"
+    height="1em"
+    width="1em"
+    className={className}
+  >
+    <path d="M6 18.7V21a1 1 0 01-2 0v-5a1 1 0 011-1h5a1 1 0 110 2H7.1A7 7 0 0019 12a1 1 0 112 0 9 9 0 01-15 6.7zM18 5.3V3a1 1 0 012 0v5a1 1 0 01-1 1h-5a1 1 0 010-2h2.9A7 7 0 005 12a1 1 0 11-2 0 9 9 0 0115-6.7z" />
+  </svg>
+);
+const WarningIcon: React.FC<SvgIconProps> = ({ className }) => (
+  <svg
+    viewBox="0 0 1024 1024"
+    fill="currentColor"
+    height="1em"
+    width="1em"
+    className={className}
+  >
+    <path d="M464 720a48 48 0 1096 0 48 48 0 10-96 0zm16-304v184c0 4.4 3.6 8 8 8h48c4.4 0 8-3.6 8-8V416c0-4.4-3.6-8-8-8h-48c-4.4 0-8 3.6-8 8zm475.7 440l-416-720c-6.2-10.7-16.9-16-27.7-16s-21.6 5.3-27.7 16l-416 720C56 877.4 71.4 904 96 904h832c24.6 0 40-26.6 27.7-48zm-783.5-27.9L512 239.9l339.8 588.2H172.2z" />
+  </svg>
+);
+
+export {
+  ThreeDotIcon,
+  EditIcon,
+  DetailIcon,
+  TrashIcon,
+  LeftArrowIcon,
+  RightArrowIcon,
+  SearchIcon,
+  PlusIcon,
+  ArrowButtonIcon,
+  LogoutIcon,
+  CloseSidebarIcon,
+  ReponsiveSidebarIcon,
+  DropdownSidebarMenuIcon,
+  CloseButtonIcon,
+  LeftArrowIcon2,
+  RefreshIcon,
+  WarningIcon,
+};
