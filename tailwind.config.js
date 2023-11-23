4bf26485319d8347619df0e8e5632863ee6ce191
Shouta002 <reynaldilight16@gmail.com>
2023-11-23 08:31:57 +0800

    Question page modified

--- tailwind.config.js
+++ tailwind.config.js
@@ -28,6 +28,16 @@ export default {
       fontFamily: {
         poppins: ['Poppins'],
       },
+
+      keyframes: {
+        fadeIn: {
+          '0%': { opacity: 0 },
+          '100%': { opacity: 100 },
+        },
+      },
+      animation: {
+        fadeIn: 'fadeIn 0.5s ease-in-out backwards',
+      },
     },
   },
   plugins: [scrollbarPlugin],
