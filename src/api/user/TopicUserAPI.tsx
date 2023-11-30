2f6d805f3f7fd74b9b344c1d9fd41b76dfb4ded6
Muhthoriqas <muhthoriqas1719@gmail.com>
2023-11-30 15:50:48 +0800

    Update API User

--- src/api/user/TopicUserAPI.tsx
+++ src/api/user/TopicUserAPI.tsx
@@ -1,19 +1,19 @@
 // Import Helpers
 import { RequestApi } from '../../helpers/RequestApi';
-import TokenHelper from '../../helpers/TokenHelpers';
+// import TokenHelper from '../../helpers/TokenHelpers';
 
 // GET TopicUser
 const getTopicUser = async () => {
   try {
-    const token = TokenHelper();
+    // const token = TokenHelper();
 
-    const headerToken = {
-      Authorization: `Bearer ${token}`,
-    };
+    // const headerToken = {
+    //   Authorization: `Bearer ${token}`,
+    // };
 
     const responseGetTopicUser = await RequestApi(
       'GET',
-      'faq/topics',
+      'faq',
       {},
       {},
       'Mengambil topic user'
@@ -27,97 +27,51 @@ const getTopicUser = async () => {
 };
 
 // GET Detail TopicUser
-const getDetailTopicUser = async (slug: any) => {
+const getListQuestionByTopic = async (slug: any) => {
   try {
-    const token = TokenHelper();
+    // const token = TokenHelper();
 
-    const headerToken = {
-      Authorization: `Bearer ${token}`,
-    };
+    // const headerToken = {
+    //   Authorization: `Bearer ${token}`,
+    // };
 
     const reponseGetDetailTopicUser = await RequestApi(
       'GET',
-      `faq/topics/${slug}`,
+      `faq/${slug}`,
       {},
-      headerToken,
-      'Mengambil detail topic user'
+      {},
+      'Mengambil list question by topic'
     );
 
     return reponseGetDetailTopicUser;
   } catch (error) {
-    console.error('Terjadi kesalahan saat mengambil data topicUser:', error);
-    throw error;
-  }
-};
-
-// POST TopicUser
-const addTopicUser = async (formData: any) => {
-  try {
-    const token = TokenHelper();
-
-    const headerToken = {
-      Authorization: `Bearer ${token}`,
-    };
-
-    const reponseAddTopicUser = await RequestApi(
-      'POST',
-      'faq/topics',
-      formData,
-      headerToken,
-      'Membuat topic user'
+    console.error(
+      'Terjadi kesalahan saat mengambil data  list question by topic:',
+      error
     );
-
-    return reponseAddTopicUser;
-  } catch (error) {
-    console.error('Kesalahan saat membuat topicUser:', error);
     throw error;
   }
 };
 
-// PUT TopicUser
-const updateTopicUser = async (slug: any, TopicUserData: any) => {
+const getDetailQuestion = async (slug_topic: any, slug_question: string) => {
   try {
-    const token = TokenHelper();
+    // const token = TokenHelper();
 
-    const headerToken = {
-      Authorization: `Bearer ${token}`,
-    };
+    // const headerToken = {
+    //   Authorization: `Bearer ${token}`,
+    // };
 
-    const reponseUpdateTopicUser = await RequestApi(
-      'PUT',
-      `faq/topics/${slug}`,
-      TopicUserData,
-      headerToken,
-      'Memperbarui topic user'
-    );
-
-    return reponseUpdateTopicUser;
-  } catch (error) {
-    console.error('Kesalahan saat memperbarui topic user:', error);
-    throw error;
-  }
-};
-
-// DELETE TopicUser
-const deleteTopicUser = async (slug: any) => {
-  try {
-    const token = TokenHelper();
-
-    const headerToken = {
-      Authorization: `Bearer ${token}`,
-    };
-
-    const reponseDeleteTopicUser = await RequestApi(
-      'DELETE',
-      `faq/topics/${slug}`,
-      null,
+    const reponseGetDetailTopicUser = await RequestApi(
+      'GET',
+      `faq/${slug_topic}/${slug_question}`,
       {},
-      'Menghapus topic user'
+      {},
+      'Mengambil detail question'
     );
 
-    return reponseDeleteTopicUser;
+    return reponseGetDetailTopicUser;
   } catch (error) {
-    console.error('Kesalahan saat menghapus topic user:', error);
+    console.error('Terjadi kesalahan saat mengambil detail question:', error);
     throw error;
   }
 };
@@ -125,18 +79,18 @@ const deleteTopicUser = async (slug: any) => {
 // Search TopicUser
 const searchTopicUser = async (searchInput: any) => {
   try {
-    const token = TokenHelper();
+    // const token = TokenHelper();
 
-    const headerToken = {
-      Authorization: `Bearer ${token}`,
-    };
+    // const headerToken = {
+    //   Authorization: `Bearer ${token}`,
+    // };
 
     const responseSearchTopicUser = await RequestApi(
       'GET',
-      `faq/topics?search=${searchInput}`,
+      `faq?search=${searchInput}`,
       null,
       {},
-      'Mencari topicUser'
+      'Mencari FAQ'
     );
 
     return responseSearchTopicUser;
@@ -146,37 +100,9 @@ const searchTopicUser = async (searchInput: any) => {
   }
 };
 
-// Change Is Active TopicUser
-const changeIsActiveTopicUser = async (idIsActive: any, newIsActive: any) => {
-  try {
-    const token = TokenHelper();
-
-    const headerToken = {
-      Authorization: `Bearer ${token}`,
-    };
-    const sendData = { is_active: newIsActive };
-
-    const responseIsActiveChange = await RequestApi(
-      'PUT',
-      `topic-user/is_active/${idIsActive}`,
-      sendData,
-      headerToken,
-      'Mengubah Active Topic User'
-    );
-
-    return responseIsActiveChange;
-  } catch (error) {
-    console.error('Kesalahan saat mengubah Active Topic User:', error);
-    throw error;
-  }
-};
-
 export {
   getTopicUser,
-  getDetailTopicUser,
-  addTopicUser,
-  updateTopicUser,
-  deleteTopicUser,
+  getListQuestionByTopic,
+  getDetailQuestion,
   searchTopicUser,
-  changeIsActiveTopicUser,
 };
