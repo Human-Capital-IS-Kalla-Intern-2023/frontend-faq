96f6ae45b8331ddfee60aa951f3b3de0b12b6a2a
Muhthoriqas <muhthoriqas1719@gmail.com>
2023-12-04 02:44:00 +0800

    change pureBlack to black and add featch priority

--- src/components/buttons/CancelButton.tsx
+++ src/components/buttons/CancelButton.tsx
@@ -8,7 +8,7 @@ const CancelButton: React.FC<CancelButtonProps> = ({ title, onClick }) => {
   return (
     <button
       aria-label="Cancel"
-      className="px-1 py-2 mr-2 duration-300 bg-transparent rounded-md lg:text-lg text-pureBlack lg:px-4 lg:py-2 lg:mr-4 bg-stone-300 hover:text-pureBlack hover:bg-slate-400 lg:hover:scale-[1.03]"
+      className="px-1 py-2 mr-2 duration-300 bg-transparent rounded-md lg:text-lg text-black lg:px-4 lg:py-2 lg:mr-4 bg-stone-300 hover:text-black hover:bg-slate-400 lg:hover:scale-[1.03]"
       onClick={onClick}
     >
       {title ? title : 'Cancel'}
