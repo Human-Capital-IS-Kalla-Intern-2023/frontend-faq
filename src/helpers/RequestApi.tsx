a88e07bc5695bd77e3516f4024813cd909abb9f3
Muhthoriqas <muhthoriqas1719@gmail.com>
2023-12-06 16:55:05 +0800

    use .env at api route

--- src/helpers/RequestApi.tsx
+++ src/helpers/RequestApi.tsx
@@ -1,7 +1,7 @@
 // Library & Package Import
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:8000/api';
+const API_BASE_URL = import.meta.env.VITE_DEV_URL_API;
 
 const handleErrorResponse = (error: any, action: string) => {
   console.error(`Error: saat ${action}`, error);
