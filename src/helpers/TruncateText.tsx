55c04e7649e11e78f24ff8d78eafb4af566aef1b
Muhthoriqas <muhthoriqas1719@gmail.com>
2023-12-20 14:52:17 +0800

    refresh gitignore

--- src/helpers/TruncateText.tsx
+++
@@ -1,3 +0,0 @@
-export const TruncateText = (text: string, maxLength: number) => {
-  return text?.length > maxLength ? text.substring(0, maxLength) + '...' : text;
-};
--- src/pages/admin/DashboardAdmin.tsx
+++
@@ -1,9 +0,0 @@
-const DashboardAdmin = () => {
-  return (
-    <div className="flex items-center justify-center h-screen text-3xl font-bold text-center">
-      <div className="">Selamat Datang</div>
-    </div>
-  );
-};
-
-export default DashboardAdmin;
