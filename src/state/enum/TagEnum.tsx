f04987975f015965c23e10c5d9bc14ad94afdf9e
Muhthoriqas <muhthoriqas1719@gmail.com>
2023-12-20 14:54:01 +0800

    refresh gitnore

--- src/state/enum/TagEnum.tsx
+++
@@ -1,9 +0,0 @@
-export enum tagEnum {
-  TEXT = 'text',
-  TEXTAREA = 'textarea',
-  SELECT = 'select',
-  CHECKBOX = 'checkbox',
-  ISACTIVE = 'is_active',
-  ESCAPE = 'Escape',
-  NUMBER = 'number',
-}
