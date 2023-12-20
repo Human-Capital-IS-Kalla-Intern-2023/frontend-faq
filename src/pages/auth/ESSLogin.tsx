5633f741dc358302f3ea1639b70faf3475dfb54f
Muhthoriqas <muhthoriqas1719@gmail.com>
2023-12-20 15:05:14 +0800

    Update logic if user already have access token

--- src/pages/auth/ESSLogin.tsx
+++ src/pages/auth/ESSLogin.tsx
@@ -1,9 +1,21 @@
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import LoginCard from '../../components/cards/LoginCard';
-
+import Cookies from 'js-cookie';
 const ESSLogin = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const token = Cookies.get('access_token');
+
+    if (token) {
+      navigate('/home');
+    }
+  }, [navigate]);
+
   return (
     <>
-      <LoginCard></LoginCard>
+      <LoginCard />
     </>
   );
 };
